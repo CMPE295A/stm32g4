1a9dae678f1ea958ae0c04ff79cbe23259ee1de0
jasdipsekhon <jasdipsekhon@gmail.com>
2023-04-29 01:48:25 -0700

    added I2C clock config and started state machine

--- Core/Src/driver_i2c.c
+++ Core/Src/driver_i2c.c
@@ -1,25 +1,42 @@
 /*
- * driver_i2c.c
+ * i2c.h
  *
  *  Created on: March 21, 2023
  *      Author: Jasdip Sekhon
  */
 
-#include "driver_i2c.h"
+#include "i2c.h"
 
-typedef struct {
-  I2C_TypeDef *const registers; // STM32 memory mapped registers for the I2C bus
+typedef enum {
+    I2C__STATE_IDLE,
+    I2C__STATE_START,
+    I2C__STATE_SEND_SLAVE_ADDR,
+	I2C__STATE_REG_ADDR,
+	I2C__STATE_REPEATED_START_CONDITION,
+    I2C__STATE_DATA_RECEIVED,
+    I2C__STATE_STOP,
+	I2C__STATE_ERROR,
+} i2c_state_t;
 
-  uint8_t slave_address;
-  uint8_t starting_slave_memory_address;
+volatile i2c_state_t i2c_state = I2C_STATE_IDLE;
+static uint8_t data[255];
 
-  uint8_t *read;        // Used for reading I2C slave device
-  const uint8_t *write; // Used for writing data to the I2C slave device
-  size_t number_of_bytes_to_transfer;
-} i2c_s;
+void I2C1_EV_IRQHandler(void) {
+	switch(i2c_state) {
+	case I2C__STATE_IDLE:
+		i2c_state = I2C__STATE_START;
+		break;
+
+	case I2C__STATE_START:
+		while (!(I2C1->ISR & I2C_ISR_BUSY));
+		I2C1->CR2 |= I2C_CR2_START;
+		i2c_state = I2C__STATE_SEND_SLAVE_ADDR;
+		break;
+	}
+}
 
 // I2C1 SDA = PB7, SCL = PA15
-static void gpio_init() {
+static void gpio_init(void) {
 	const uint32_t i2c_mode = 0x2;
 	const uint32_t pull_up = 0x1;
 	const uint32_t clear = 0x3;
@@ -41,24 +58,60 @@ static void gpio_init() {
 	GPIOA->PUPDR |= (pull_up << 30);       // Enable pull-up resistor for PA15
 }
 
-static void enable_GPIO_and_I2C_clock() {
+static void enable_GPIO_and_I2C_clock(void) {
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOBEN; // enable peripheral clock for GPIO A and B
 	RCC->APB1ENR1 |= RCC_APB1ENR1_I2C1EN; // enable peripheral clock for I2C1
 }
 
-void i2c__initialize() {
+static void congifure_I2C_clock(void) {
+	const uint32_t prescaler = 4;
+	const uint32_t SCLDEL = 4;
+	const uint32_t SDADEL = 2;
+	const uint32_t SCLH = 67;
+	const uint32_t SCLL = 134;
+
+	// Configure I2C1_TIMINGR for 100KHz speed
+	I2C1->TIMINGR = (prescaler << 28) |
+					(SCLDEL << 20) |
+					(SDADEL << 16) |
+					(SCLH << 8) |
+					(SCLL);
+}
+
+void i2c__initialize(void) {
 	enable_GPIO_and_I2C_clock();
 	gpio_init();
 
 	I2C1->CR1 &= ~I2C_CR1_PE;
-	I2C1->TIMINGR = 0x00B01A4B;          // TODO: need to change timer value
+	congifure_I2C_clock();
 	I2C1->CR1 &= ~I2C_CR1_ANFOFF;        // Enable analog filter
 	I2C1->CR1 &= ~I2C_CR1_DNF;
 	I2C1->CR1 |= 0x3 << I2C_CR1_DNF_Pos; // Set digital noise filter
-	I2C1->OAR1 = 0;
-	I2C1->OAR2 = 0;
-	I2C1->CR2 = 0;
-	I2C1->CR1 &= I2C_CR1_PE;
+	I2C1->CR1 &= ~I2C_CR1_NOSTRETCH; 	 // Enable clock stretching
+	I2C1->CR2 &= ~I2C_CR2_NACK;			 // Enable NACK generation
+	I2C1->OAR1 = 0x0;
+	I2C1->OAR2 = 0x0;
+	I2C1->CR2 = 0x0;
+	I2C1->CR1 |= I2C_CR1_PE;
+
+	// Enable interrupts for I2C events
+	NVIC_EnableIRQ(I2C1_EV_IRQn);
+	NVIC_SetPriority(I2C1_EV_IRQn, 2);
+	I2C1->CR1 |= I2C_CR1_RXIE; // enable Receive Data Register not empty interrupt
+}
+
+bool i2c__detect(uint8_t slave_address)
+{
+	bool detected = false;
+    I2C1->CR2 = (slave_address << 1) | I2C_CR2_START; // Send start condition
+    while (!(I2C1->ISR & I2C_ISR_BUSY)); // Wait for start condition to be sent
+    while (!(I2C1->ISR & I2C_ISR_ADDR)); // Wait for end of address transmission
+    (void)I2C1->SR1; // Clear ADDR flag by reading SR1 and SR2 registers
+    (void)I2C1->SR2;
+    if ((I2C1->ISR & I2C_ISR_NACKF) || (I2C1->ISR & I2C_ISR_ARLO)) { // ACK received
+    	detected = true;
+    }
+    return detected;
 }
 
 uint8_t i2c__read_slave_data(uint8_t slave_address, uint8_t register_address) {
@@ -74,13 +127,24 @@ uint8_t i2c__read_slave_data(uint8_t slave_address, uint8_t register_address) {
 }
 
 void i2c__write_slave_data(uint8_t slave_address, uint8_t register_address, uint8_t data) {
-	while((I2C1->ISR & I2C_ISR_BUSY) == I2C_ISR_BUSY); // Wait until I2C bus is ready
-        I2C1->CR2 = (slave_address << 1) | I2C_CR2_AUTOEND | ((0x2 << I2C_CR2_NBYTES_Pos)); // Send start condition and slave address
-    	I2C1->CR2 |= I2C_CR2_START;
-    	while((I2C1->ISR & I2C_ISR_TXIS) == 0);  // Wait for TXIS flag to be set (transmit register address)
-    	I2C1->TXDR = reg_address;                // Write the register address to the DR register
-    	while((I2C1->ISR & I2C_ISR_TXIS) == 0);  // Wait for TXIS flag to be set (transmit data)
-    	I2C1->TXDR = data; 						 // Write the data to the DR register
-    	while((I2C1->ISR & I2C_ISR_STOPF) == 0); // Wait for STOP flag to be set
-    	I2C1->ICR |= I2C_ICR_STOPCF;             // Clear the STOP flag by writing 0 to it
+    while((I2C1->ISR & I2C_ISR_BUSY) == I2C_ISR_BUSY); // Wait until I2C bus is ready
+    I2C1->CR2 = (slave_address << 1) | I2C_CR2_AUTOEND | ((0x2 << I2C_CR2_NBYTES_Pos)); // Send start condition and slave address
+    I2C1->CR2 |= I2C_CR2_START;
+    while((I2C1->ISR & I2C_ISR_TXIS) == 0);  // Wait for TXIS flag to be set (transmit register address)
+    I2C1->TXDR = reg_address;                // Write the register address to the DR register
+    while((I2C1->ISR & I2C_ISR_TXIS) == 0);  // Wait for TXIS flag to be set (transmit data)
+    I2C1->TXDR = data; 						 // Write the data to the DR register
+    while((I2C1->ISR & I2C_ISR_STOPF) == 0); // Wait for STOP flag to be set
+    I2C1->ICR |= I2C_ICR_STOPCF;             // Clear the STOP flag by writing 0 to it
 }
+
+void i2c1_event_irq_handler() {
+    if (I2C1->ISR & I2C_ISR_RXNE) {
+        i2c_rx_buffer[i2c_rx_index++] = I2C1->RXDR;
+    }
+}
+
+//void i2c__HAL_read_slave_data(uint8_t deviceAddress, uint8_t* data, uint16_t dataSize) {
+//    HAL_I2C_Master_Receive(&hi2c1, deviceAddress, data, dataSize, 1000);
+//}
+
